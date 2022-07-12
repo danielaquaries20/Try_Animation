15d849dbdad800cbeaf5d862698ef32330517427
prata <danielaquaries@gmail.com>
2022-07-12 15:05:08 +0700

    Menambahkan Map Navigation Driver Direction.

--- app/src/main/java/com/example/tryanimation/MainActivity.kt
+++ app/src/main/java/com/example/tryanimation/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.tryanimation
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var ivAnimation: ImageView
+    private lateinit var btnToMap: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         ivAnimation = findViewById(R.id.ivImageAnimation)
+        btnToMap = findViewById(R.id.btnShow)
 
 //        var runnable: Runnable? = null
 //        runnable = Runnable {
@@ -27,6 +32,20 @@ class MainActivity : AppCompatActivity() {
 //            }.withEndAction(runnable).start()
 //        }
 //        runnable.run()
+//        Uri.parse("google.navigation:q=22.659239,88.435534&mode=l")
+        val tempat = "SMP+Negeri+3+Ungaran"
+        btnToMap.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("google.navigation:q=$tempat&mode=d")
+            )
+            intent.setPackage("com.google.android.apps.maps")
+            startActivity(intent)
+//            intent.resolveActivity(packageManager)?.let {
+//                startActivity(intent)
+//            }
+        }
+
 
         ivAnimation.animate().apply {
             duration = 1000
