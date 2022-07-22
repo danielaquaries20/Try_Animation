908258d7ced5547d5294f910fdf3d670e5da6436
prata <danielaquaries@gmail.com>
2022-07-22 15:51:32 +0700

    Menambahkan BottomSheet dan AlertDialog 'Try'.

--- app/src/main/java/com/example/tryanimation/MainActivity.kt
+++ app/src/main/java/com/example/tryanimation/MainActivity.kt
@@ -1,46 +1,93 @@
 package com.example.tryanimation
 
 import android.content.Intent
+import android.graphics.Typeface
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import smartdevelop.ir.eram.showcaseviewlib.GuideView
+import smartdevelop.ir.eram.showcaseviewlib.config.DismissType
+import smartdevelop.ir.eram.showcaseviewlib.config.Gravity
+import smartdevelop.ir.eram.showcaseviewlib.config.PointerType
+
 
 class MainActivity : AppCompatActivity() {
 
+    /*region Inisialisai*/
     private lateinit var ivAnimation: ImageView
     private lateinit var btnToMap: Button
     private lateinit var btnNext: Button
     private lateinit var btnMotionLayout: Button
     private lateinit var btnCoordinatorLayout: Button
+    private lateinit var btnBtmSheetShow: Button
+    private lateinit var btnAlerDialogShow: Button
+    private lateinit var btnTryCenterConstraint: Button
+    /*endregion*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*region Deklarasi*/
         ivAnimation = findViewById(R.id.ivImageAnimation)
         btnToMap = findViewById(R.id.btnShow)
         btnNext = findViewById(R.id.btnNext)
         btnMotionLayout = findViewById(R.id.btnMotionLayout)
         btnCoordinatorLayout = findViewById(R.id.btnCoordinatorLayout)
+        btnBtmSheetShow = findViewById(R.id.btnBtmSheetShow)
+        btnAlerDialogShow = findViewById(R.id.btnAlertDialogShow)
+        btnTryCenterConstraint = findViewById(R.id.btnTryCenterConstraintLayout)
+        /*endregion*/
+
+        /*region Action*/
+        val tempat = "SMP+Negeri+3+Ungaran"
+        btnToMap.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("google.navigation:q=$tempat&mode=d")
+            )
+            intent.setPackage("com.google.android.apps.maps")
+            startActivity(intent)
+//            intent.resolveActivity(packageManager)?.let {
+//                startActivity(intent)
+//            }
+        }
 
         btnNext.setOnClickListener {
-            startActivity(Intent(this,AnimationOneActivity::class.java))
+            startActivity(Intent(this, AnimationOneActivity::class.java))
             overridePendingTransition(R.anim.transition_from_bottom, R.anim.transition_to_top)
         }
 
         btnMotionLayout.setOnClickListener {
-            startActivity(Intent(this,MotionLayoutOneActivity::class.java))
-            overridePendingTransition(R.anim.transition_from_right,R.anim.transition_to_left)
+            startActivity(Intent(this, MotionLayoutOneActivity::class.java))
+            overridePendingTransition(R.anim.transition_from_right, R.anim.transition_to_left)
         }
 
         btnCoordinatorLayout.setOnClickListener {
-            startActivity(Intent(this,CoordinatorActivity::class.java))
-            overridePendingTransition(R.anim.transition_zoom_in,R.anim.transition_state)
+            startActivity(Intent(this, CoordinatorActivity::class.java))
+            overridePendingTransition(R.anim.transition_zoom_in, R.anim.transition_state)
         }
 
+        btnBtmSheetShow.setOnClickListener {
+            bottomSheetTry()
+        }
 
+        btnAlerDialogShow.setOnClickListener {
+            alertDialogTry()
+        }
+
+        btnTryCenterConstraint.setOnClickListener {
+            startActivity(Intent(this, TryCenterViewActivity::class.java))
+        }
+
+        /*endregion*/
+
+        /*region Animation*/
 //        var runnable: Runnable? = null
 //        runnable = Runnable {
 //            ivAnimation.animate().apply {
@@ -55,19 +102,6 @@ class MainActivity : AppCompatActivity() {
 //        }
 //        runnable.run()
 //        Uri.parse("google.navigation:q=22.659239,88.435534&mode=l")
-        val tempat = "SMP+Negeri+3+Ungaran"
-        btnToMap.setOnClickListener {
-            val intent = Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse("google.navigation:q=$tempat&mode=d")
-            )
-            intent.setPackage("com.google.android.apps.maps")
-            startActivity(intent)
-//            intent.resolveActivity(packageManager)?.let {
-//                startActivity(intent)
-//            }
-        }
-
 
         ivAnimation.animate().apply {
             duration = 1000
@@ -88,7 +122,58 @@ class MainActivity : AppCompatActivity() {
                 translationYBy(-20f)
             }
         }.start()
+        /*endregion*/
 
+        guideViewFirst()
 
     }
+
+    private fun bottomSheetTry() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        val bottomSheetTry = LayoutInflater.from(applicationContext).inflate(
+            R.layout.bottom_sheet_try, findViewById(R.id.btmSheetTry)
+        )
+        bottomSheetDialog.setContentView(bottomSheetTry)
+        bottomSheetDialog.show()
+
+        bottomSheetTry.findViewById<Button>(R.id.btnBtmShtDismiss)
+            .setOnClickListener {
+                bottomSheetDialog.dismiss()
+            }
+
+    }
+
+    private fun guideViewFirst() {
+        GuideView.Builder(this)
+            .setTitle("Button Google Map")
+            .setContentText("Jika Button ini di klik\n maka akan pindah ke Google Map")
+            .setTargetView(btnToMap)
+            .setGravity(Gravity.center)
+            .setCircleIndicatorSize(10f)
+            .setCircleStrokeIndicatorSize(2f)
+            .setCircleInnerIndicatorSize(5f)
+            .setContentTypeFace(Typeface.MONOSPACE)//optional
+            .setTitleTypeFace(Typeface.SANS_SERIF)//optional
+            .setDismissType(DismissType.anywhere) //optional - default dismissible by TargetView
+            .setPointerType(PointerType.circle)
+            .build()
+            .show()
+    }
+
+    private fun alertDialogTry() {
+        val alertDialogTry =
+            LayoutInflater.from(this)
+                .inflate(R.layout.alert_dialog_try, findViewById(R.id.alertDialogTry))
+
+
+        val alertDialogBuilder = AlertDialog.Builder(this)
+            .setView(alertDialogTry)
+
+        val theAlertDialog = alertDialogBuilder.show()
+
+        alertDialogTry.findViewById<Button>(R.id.btnAlertDialogDismiss).setOnClickListener {
+            theAlertDialog.dismiss()
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/tryanimation/TryCenterViewActivity.kt
@@ -0,0 +1,31 @@
+package com.example.tryanimation
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class TryCenterViewActivity : AppCompatActivity() {
+
+//    private lateinit var view1: View
+//    private lateinit var view2: View
+//    private lateinit var btnTry: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_try_center_view)
+
+//        view1 = findViewById(R.id.vTryCenter1)
+//        view2 = findViewById(R.id.vTryCenter2)
+//        val viewNya = findViewById<View>(R.id.btnTryCenterHello)
+
+//        val heightContent = findViewById<View>(R.id.btnTryCenterHello).height
+//
+//        Log.d("CariHeight", "Height Content : $heightContent")
+//
+//        view1.minimumHeight = heightContent
+//        view2.minimumHeight = heightContent
+
+
+    }
+}
