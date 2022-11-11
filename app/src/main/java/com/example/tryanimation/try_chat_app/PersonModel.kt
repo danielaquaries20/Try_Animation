e91eae97762cae3533c92d579283e2db57372a20
prata <danielaquaries@gmail.com>
2022-11-11 15:05:54 +0700

    Menambahkan Send Message From Me.

--- app/src/main/java/com/example/tryanimation/try_chat_app/PersonModel.kt
+++ app/src/main/java/com/example/tryanimation/try_chat_app/PersonModel.kt
@@ -1,12 +1,12 @@
 package com.example.tryanimation.try_chat_app
 
 data class PersonModel(
-    val id: Int?,
-    val nama: String?,
-    val nomer: String?,
-    val panggilan: String?,
-    val date: String?,
-    val type: Int?,
-    val chat: String?,
-    val time: String?,
+    val id: Int? = null,
+    val nama: String? = null,
+    val nomer: String? = null,
+    val panggilan: String? = null,
+    val date: String? = null,
+    val type: Int? = null,
+    val chat: String? = null,
+    val time: String? = null,
 )
--- app/src/main/java/com/example/tryanimation/try_chat_app/TryChatActivity.kt
+++ app/src/main/java/com/example/tryanimation/try_chat_app/TryChatActivity.kt
@@ -1,10 +1,9 @@
 package com.example.tryanimation.try_chat_app
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.widget.EditText
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
@@ -17,8 +16,10 @@ class TryChatActivity : AppCompatActivity() {
     private lateinit var rvChat: RecyclerView
     private lateinit var ivBack: ImageView
     private lateinit var cardSendMessage: CardView
+    private lateinit var etTypeMessage: EditText
 
     private var chatArray: ArrayList<PersonModel> = ArrayList()
+    private var dummyChatArray: ArrayList<PersonModel> = ArrayList()
     private var chatAdapter: ChatAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,42 +29,84 @@ class TryChatActivity : AppCompatActivity() {
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         window.statusBarColor = Color.TRANSPARENT*/
 
-
         rvChat = findViewById(R.id.rvChat)
         ivBack = findViewById(R.id.ivBack)
         cardSendMessage = findViewById(R.id.cardSendMessage)
+        etTypeMessage = findViewById(R.id.etTypeMessage)
 
+
+        initView()
+        dummyData()
+
+        initOnClick()
+    }
+
+    private fun initOnClick() {
         ivBack.setOnClickListener { onBackPressed() }
+
         cardSendMessage.setOnClickListener {
-            Toast.makeText(
+            sendMessage()
+            /*Toast.makeText(
                 this,
                 "Send Message",
                 Toast.LENGTH_SHORT
-            ).show()
+            ).show()*/
         }
+    }
 
-        dummyData()
-        initView()
+    private fun sendMessage() {
+        val message = etTypeMessage.text.trim().toString()
+        val chatLastIndex = if (chatArray.lastIndex == -1) null else chatArray.lastIndex
+        val lastChatId = if (chatLastIndex == null) 0 else chatArray[chatLastIndex].id
+        val newId = lastChatId?.plus(1)
+        if (message.isNotEmpty()) {
+            dummyChatArray.add(
+                PersonModel(
+                    id = newId,
+                    nama = "Daniel Aquaries Pratama",
+                    nomer = "098765237642",
+                    panggilan = "Daniel",
+                    date = "11 November 2022",
+                    type = 1,
+                    chat = message,
+                    time = "00.00"
+                )
+            )
+            setChat()
+            chatAdapter?.itemCount?.let { chatAdapter?.notifyItemInserted(it) }
+            etTypeMessage.setText("")
+//            Log.d("Chat", "data: $chatArray")
+        }
+    }
+
+    private fun setChat() {
+        chatArray.clear()
+        for (data in dummyChatArray.withIndex()) {
+            if (data.index == 0) {
+                chatArray.add(PersonModel(type = 0, date = data.value.date))
+            } else if (data.value.date?.substring(0, 10) != dummyChatArray[data.index - 1].date?.substring(0, 10)
+            ) {
+                chatArray.add(PersonModel(type = 0, date = data.value.date))
+            }
+            chatArray.add(data.value)
+        }
+    }
+
+    private fun initView() {
+        chatAdapter = ChatAdapter(this, chatArray)
+        rvChat.layoutManager = LinearLayoutManager(this)
+        rvChat.adapter = chatAdapter
+        ViewCompat.setNestedScrollingEnabled(rvChat, false)
     }
 
     private fun dummyData() {
         val dummyData = arrayOf(
             PersonModel(
-                id = 0,
-                nama = null,
-                nomer = null,
-                panggilan = null,
-                date = "10 November 2022",
-                type = 0,
-                chat = null,
-                time = null
-            ),
-            PersonModel(
                 id = 1,
                 nama = "Daniel Aquaries Pratama",
                 nomer = "098765237642",
                 panggilan = "Daniel",
-                date = "10-November-2022",
+                date = "10 November 2022",
                 type = 1,
                 chat = "Hallo, apa kabar?",
                 time = "10.00"
@@ -73,7 +116,7 @@ class TryChatActivity : AppCompatActivity() {
                 nama = "Elysia",
                 nomer = "8905627482",
                 panggilan = "Elysia",
-                date = "10-November-2022",
+                date = "10 November 2022",
                 type = 2,
                 chat = "Hai, kabarku baik\nKalau kamu dan?",
                 time = "10.01"
@@ -83,7 +126,7 @@ class TryChatActivity : AppCompatActivity() {
                 nama = "Daniel Aquaries Pratama",
                 nomer = "098765237642",
                 panggilan = "Daniel",
-                date = "10-November-2022",
+                date = "10 November 2022",
                 type = 1,
                 chat = "Baik juga kok El.\nEl, kamu tau nggak hari ini hari apa?",
                 time = "10.02"
@@ -93,7 +136,7 @@ class TryChatActivity : AppCompatActivity() {
                 nama = "Elysia",
                 nomer = "8905627482",
                 panggilan = "Elysia",
-                date = "10-November-2022",
+                date = "10 November 2022",
                 type = 2,
                 chat = "Lha emang hari apa?",
                 time = "10.03"
@@ -103,7 +146,7 @@ class TryChatActivity : AppCompatActivity() {
                 nama = "Daniel Aquaries Pratama",
                 nomer = "098765237642",
                 panggilan = "Daniel",
-                date = "10-November-2022",
+                date = "10 November 2022",
                 type = 1,
                 chat = "Hari ini hari pahlawan lho.\nKamu ikut lomba nggak?",
                 time = "10.04"
@@ -113,7 +156,7 @@ class TryChatActivity : AppCompatActivity() {
                 nama = "Elysia",
                 nomer = "8905627482",
                 panggilan = "Elysia",
-                date = "10-November-2022",
+                date = "10 November 2022",
                 type = 2,
                 chat = "Oh iya, tak kira apa lho dan. hehehehehe, Kalau lomba aku nggak ikut sih.\nLha ada apa dan?",
                 time = "10.05"
@@ -123,7 +166,7 @@ class TryChatActivity : AppCompatActivity() {
                 nama = "Daniel Aquaries Pratama",
                 nomer = "098765237642",
                 panggilan = "Daniel",
-                date = "10-November-2022",
+                date = "10 November 2022",
                 type = 1,
                 chat = "Hmm, gimana kalau kita jalan jalan di domain mu? (Elysian Realm)",
                 time = "10.05"
@@ -133,7 +176,7 @@ class TryChatActivity : AppCompatActivity() {
                 nama = "Elysia",
                 nomer = "8905627482",
                 panggilan = "Elysia",
-                date = "10-November-2022",
+                date = "10 November 2022",
                 type = 2,
                 chat = "Boleh boleh, jam 2 siang ya aku tak kerumahmu terus tak ajak ke duniaku. Okey?",
                 time = "10.06"
@@ -143,7 +186,7 @@ class TryChatActivity : AppCompatActivity() {
                 nama = "Daniel Aquaries Pratama",
                 nomer = "098765237642",
                 panggilan = "Daniel",
-                date = "10-November-2022",
+                date = "10 November 2022",
                 type = 1,
                 chat = "Okey.",
                 time = "10.06"
@@ -153,7 +196,7 @@ class TryChatActivity : AppCompatActivity() {
                 nama = "Daniel Aquaries Pratama",
                 nomer = "098765237642",
                 panggilan = "Daniel",
-                date = "10-November-2022",
+                date = "10 November 2022",
                 type = 1,
                 chat = "JABCbcknckjdncjkdnsjkvkjancsukvfbsdncjsdnvjknskjvndsjcnajkncvbaxmqoiwifncrwibvqcnvnuksmxdbwbvuwjdcnwbvwjdcnubvnwicunrwuinwjucnufnchwnvwijcnvbnwjnceuvnwdjnceyvbjsdcnwjkenvuwnackjqneivnqkjxnajkncewbajkncywbvjksncuivnkjdcnyrvnkdcnuifvnksjdcnvbskjdcnfdvbjksdnvjfvnsjkvndjsvdvnsdjkdnhkjndv",
                 time = "10.20"
@@ -163,7 +206,7 @@ class TryChatActivity : AppCompatActivity() {
                 nama = "Daniel Aquaries Pratama",
                 nomer = "098765237642",
                 panggilan = "Daniel",
-                date = "10-November-2022",
+                date = "10 November 2022",
                 type = 1,
                 chat = "Sorry, typo El. Hehehehehehe",
                 time = "10.20"
@@ -174,22 +217,72 @@ class TryChatActivity : AppCompatActivity() {
                 nama = "Elysia",
                 nomer = "8905627482",
                 panggilan = "Elysia",
-                date = "10-November-2022",
+                date = "10 November 2022",
                 type = 2,
                 chat = "Hehehehhe, ok nggak papa kok",
                 time = "10.25"
             ),
+            PersonModel(
+                id = 12,
+                nama = "Elysia",
+                nomer = "8905627482",
+                panggilan = "Elysia",
+                date = "11 November 2022",
+                type = 2,
+                chat = "Halo dan",
+                time = "13.00"
+            ),
+            PersonModel(
+                id = 13,
+                nama = "Daniel Aquaries Pratama",
+                nomer = "098765237642",
+                panggilan = "Daniel",
+                date = "11 November 2022",
+                type = 1,
+                chat = "Oh, hai El\nSorry baru bales lagi ngoding.",
+                time = "13.50"
+
+            ),
+            PersonModel(
+                id = 14,
+                nama = "Elysia",
+                nomer = "8905627482",
+                panggilan = "Elysia",
+                date = "11 November 2022",
+                type = 2,
+                chat = "Oh ok, sorry kalau ganggu",
+                time = "13.51"
+            ),
+            PersonModel(
+                id = 15,
+                nama = "Daniel Aquaries Pratama",
+                nomer = "098765237642",
+                panggilan = "Daniel",
+                date = "11 November 2022",
+                type = 1,
+                chat = "Nggak papa kok El.\nLagian udah hampir selesai",
+                time = "13.51"
+
+            ),
         )
 
         chatArray.clear()
-        chatArray.addAll(dummyData)
-    }
+        dummyChatArray.clear()
+        chatAdapter?.notifyDataSetChanged()
 
-    private fun initView() {
-        chatAdapter = ChatAdapter(this, chatArray)
-        rvChat.layoutManager = LinearLayoutManager(this)
-        rvChat.adapter = chatAdapter
-        ViewCompat.setNestedScrollingEnabled(rvChat, false)
-    }
+        dummyChatArray.addAll(dummyData)
 
+        setChat()
+        /*for (data in dummyChatArray.withIndex()) {
+            if (data.index == 0) {
+                chatArray.add(PersonModel(type = 0, date = data.value.date))
+            } else if (data.value.date?.substring(0, 10) != dummyChatArray[data.index - 1].date?.substring(0, 10)
+            ) {
+                chatArray.add(PersonModel(type = 0, date = data.value.date))
+            }
+            chatArray.add(data.value)
+        }*/
+        chatAdapter?.notifyDataSetChanged()
+        /*chatArray.addAll(dummyData)*/
+    }
 }
