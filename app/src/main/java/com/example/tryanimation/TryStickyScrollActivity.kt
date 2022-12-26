94704fdaf54ad0639657b4ebb2370928235dfdae
prata <danielaquaries@gmail.com>
2022-12-26 13:56:18 +0700

    Mencoba Sticky Scroll View dengan Library Amarjain.

--- app/src/main/java/com/example/tryanimation/TryStickyScrollActivity.kt
+++ app/src/main/java/com/example/tryanimation/TryStickyScrollActivity.kt
@@ -2,10 +2,18 @@ package com.example.tryanimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class TryStickyScrollActivity : AppCompatActivity() {
+
+    private lateinit var ivBack : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_try_sticky_scroll)
+
+        ivBack = findViewById(R.id.ivBack)
+
+        ivBack.setOnClickListener { onBackPressed() }
     }
 }
