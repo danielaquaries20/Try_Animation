940b5090ca6796d16e620fedd5e6789125736461
prata <danielaquaries@gmail.com>
2022-12-26 13:24:36 +0700

    Mencoba Sticky Scroll View dengan Library Amarjain.

--- app/src/main/java/com/example/tryanimation/MainActivity.kt
+++ app/src/main/java/com/example/tryanimation/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnTryNavigationComponent: Button
     private lateinit var btnTryChat: Button
     private lateinit var btnTryDatePicker: Button
+    private lateinit var btnTryStickyView: Button
     /*endregion*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,6 +52,7 @@ class MainActivity : AppCompatActivity() {
         btnTryNavigationComponent = findViewById(R.id.btnTryNavigationComponent)
         btnTryChat = findViewById(R.id.btnTryChatApp)
         btnTryDatePicker = findViewById(R.id.btnTryDatePicker)
+        btnTryStickyView = findViewById(R.id.btnTryStickyView)
         /*endregion*/
 
         /*region Action*/
@@ -109,6 +111,10 @@ class MainActivity : AppCompatActivity() {
 
         btnTryDatePicker.setOnClickListener {
             startActivity(Intent(this, TryDatePickerActivity::class.java))
+        }
+
+        btnTryStickyView.setOnClickListener {
+            startActivity(Intent(this, TryStickyScrollActivity::class.java))
         }
 
         /*endregion*/
---
+++ app/src/main/java/com/example/tryanimation/TryStickyScrollActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tryanimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TryStickyScrollActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_try_sticky_scroll)
+    }
+}
