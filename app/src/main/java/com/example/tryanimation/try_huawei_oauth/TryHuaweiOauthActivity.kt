92bb42d4beebaee2f404f9bfc5bfc5bb8cb467a6
prata <danielaquaries@gmail.com>
2023-02-21 14:26:29 +0700

    Menambahkan Button Huawei Auth.

--- app/src/main/java/com/example/tryanimation/try_huawei_oauth/TryHuaweiOauthActivity.kt
+++ app/src/main/java/com/example/tryanimation/try_huawei_oauth/TryHuaweiOauthActivity.kt
@@ -5,19 +5,26 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tryanimation.R
+import com.huawei.hms.support.hwid.ui.HuaweiIdAuthButton
 
 class TryHuaweiOauthActivity : AppCompatActivity() {
 
     private lateinit var btnTryOauth: Button
+    private lateinit var btnHuaweiAuth: HuaweiIdAuthButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_try_huawei_oauth)
 
         btnTryOauth = findViewById(R.id.btnTry)
+        btnHuaweiAuth = findViewById(R.id.btnHuaweiAuth)
 
         btnTryOauth.setOnClickListener {
             Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        btnHuaweiAuth.setOnClickListener {
+            Toast.makeText(this, "Huawei Auth", Toast.LENGTH_SHORT).show()
         }
     }
 }
