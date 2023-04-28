6aef74bdcd91de93416dfb00d4a34929c190e670
prata <danielaquaries@gmail.com>
2023-04-28 16:44:20 +0700

    Kelas Industri Basic - Chapter 4.

---
+++ app/src/main/java/com/example/tryanimation/ui/kl_basic/chapter_4/Chapter4MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tryanimation.ui.kl_basic.chapter_4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tryanimation.R
+
+class Chapter4MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chapter4_main)
+    }
+}
