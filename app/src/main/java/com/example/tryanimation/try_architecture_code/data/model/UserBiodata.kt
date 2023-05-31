b417eed1af28e8e055642012ff94b967e2f8db51
prata <danielaquaries@gmail.com>
2023-05-31 09:34:15 +0700

    Checkpoint.

---
+++ app/src/main/java/com/example/tryanimation/try_architecture_code/data/model/UserBiodata.kt
@@ -0,0 +1,11 @@
+package com.example.tryanimation.try_architecture_code.data.model
+
+data class UserBiodata(
+    var name: String,
+    var description: String,
+    var gender: String,
+    var hometown: String,
+    var school: String,
+    var hobby: String,
+    var socialMedia: String,
+)
