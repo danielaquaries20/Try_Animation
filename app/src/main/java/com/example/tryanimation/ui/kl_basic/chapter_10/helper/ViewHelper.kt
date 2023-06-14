49a79f9b41b860d8ef33a1cff00232e94cbd7cc7
prata <danielaquaries@gmail.com>
2023-06-14 15:06:15 +0700

    Persiapan Kelas Industri Chapter 11.

---
+++ app/src/main/java/com/example/tryanimation/ui/kl_basic/chapter_10/helper/ViewHelper.kt
@@ -0,0 +1,4 @@
+package com.example.tryanimation.ui.kl_basic.chapter_10.helper
+
+class ViewHelper {
+}
---
+++ app/src/main/java/com/example/tryanimation/ui/kl_basic/chapter_11/Chapter11MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tryanimation.ui.kl_basic.chapter_11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Chapter11MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chapter11_main)
+    }
+}
