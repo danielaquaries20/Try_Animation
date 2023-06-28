cafc1b529fa9fdf06951b7113ff0877b49d28483
prata <danielaquaries@gmail.com>
2023-06-28 12:59:09 +0700

    Persiapan Kelas Industri Chapter 13 - Gallery dan Camera.

--- app/src/main/java/com/example/tryanimation/ui/kl_basic/chapter_13/test_ui/gallery_and_camera/GalleryCameraActivity.kt
+++ app/src/main/java/com/example/tryanimation/ui/kl_basic/chapter_13/test_ui/gallery_and_camera/GalleryCameraActivity.kt
@@ -1,12 +1,109 @@
 package com.example.tryanimation.ui.kl_basic.chapter_13.test_ui.gallery_and_camera
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import com.example.tryanimation.R
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import com.example.tryanimation.databinding.ActivityGalleryCameraBinding
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
 
 class GalleryCameraActivity : AppCompatActivity() {
+
+    private lateinit var photoFile: File
+
+    private lateinit var binding: ActivityGalleryCameraBinding
+
+    private var cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+                binding.ivPhoto.setImageBitmap(takenImage)
+            }
+        }
+
+    private var galleryLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val parcelFileDescriptor = contentResolver.openFileDescriptor(result?.data?.data
+                    ?: return@registerForActivityResult, "r")
+                val fileDescriptor = parcelFileDescriptor?.fileDescriptor
+                val inputStream = FileInputStream(fileDescriptor)
+
+                val outputStream = FileOutputStream(photoFile)
+
+                inputStream.use { input ->
+                    outputStream.use { output ->
+                        input.copyTo(output)
+                    }
+                }
+
+                parcelFileDescriptor?.close()
+
+                val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+                binding.ivPhoto.setImageBitmap(takenImage)
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gallery_camera)
+        binding = ActivityGalleryCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        photoFile = try {
+            creteImageFile()
+        } catch (ex: IOException) {
+            Toast.makeText(this, "Cannot use Camera", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        binding.btnCamera.setOnClickListener {
+            openCamera()
+        }
+
+        binding.btnGallery.setOnClickListener {
+            openGallery()
+        }
+
+    }
+
+    private fun openCamera() {
+        val photoURI =
+            FileProvider.getUriForFile(this, "com.example.tryanimation.fileprovider", photoFile)
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+        }
+
+        try {
+            cameraLauncher.launch(cameraIntent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "Cannot use Camera", Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        }
+    }
+
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        try {
+            galleryLauncher.launch(galleryIntent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "Cannot use Gallery", Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun creteImageFile(): File {
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("PHOTO_", ".jpg", storageDir)
     }
 }
