cbec88501f31f3ea2f147dc1c4ce7a39b3575727
prata <danielaquaries@gmail.com>
2023-07-04 11:59:21 +0700

    Mencoba Read Characteristic - Mendapatkan Value Byte Array.

--- app/src/main/java/com/example/tryanimation/try_bluetooth_connection/bluetooth_trial/MainBluetoothActivity.kt
+++ app/src/main/java/com/example/tryanimation/try_bluetooth_connection/bluetooth_trial/MainBluetoothActivity.kt
@@ -18,6 +18,7 @@ import androidx.core.content.ContextCompat
 import com.crocodic.core.base.activity.CoreActivity
 import com.crocodic.core.base.adapter.CoreListAdapter
 import com.crocodic.core.extension.checkLocationPermission
+import com.crocodic.core.extension.snack
 import com.crocodic.core.extension.snacked
 import com.crocodic.core.extension.tos
 import com.example.tryanimation.R
@@ -72,7 +73,7 @@ class MainBluetoothActivity :
         super.onClick(v)
         when (v) {
             binding.fabCast -> binding.root.snacked("Casting")
-            binding.fabCheckHeart -> binding.root.snacked("Check Heart")
+            binding.fabCheckHeart -> readStaticChar()
             binding.fabCheckSport -> binding.root.snacked("Check Sport")
         }
     }
@@ -294,6 +295,7 @@ class MainBluetoothActivity :
                     binding.root.snacked("Service Discovered: ${gatt?.services}")
                     Timber.tag("ConnectGatt").d("GATT_SUCCESS: $status")
                     Timber.tag("ConnectGatt").d("GATT: ${gatt?.services}")
+                    displayGattService(gatt?.services)
                 }
                 else -> Timber.tag("ConnectGatt").d("onServicesDiscovered_received: $status")
             }
@@ -304,15 +306,110 @@ class MainBluetoothActivity :
             characteristic: BluetoothGattCharacteristic?,
             status: Int,
         ) {
-            Timber.tag("ConnectGatt").d("onCharacteristicRead: $status")
+            Timber.tag("CharacteristicGatt").d("onCharacteristicRead: $status")
+            val data: ByteArray? = characteristic?.value
+            if (data?.isNotEmpty() == true) {
+                val hexString: String = data.joinToString(separator = " ") {
+                    String.format("%02X", it)
+                }
+                Timber.tag("CharacteristicGatt").d("CHAR_VALUE: $data and $hexString")
+                binding.root.snacked("CHAR_VALUE: $data and $hexString")
+            }
         }
     }
     /*endregion*/
+
+    private lateinit var mGattCharacteristics: MutableList<BluetoothGattCharacteristic>
+
+    private fun displayGattService(gattServices: List<BluetoothGattService>?) {
+        if (gattServices == null || gattServices.isEmpty()) {
+            binding.root.snacked("There is no any Service")
+            return
+        }
+
+        var uuid: String?
+        val gattServiceData: MutableList<HashMap<String, String>> = mutableListOf()
+        val gattCharacteristicData: MutableList<ArrayList<HashMap<String, String>>> = mutableListOf()
+        mGattCharacteristics = mutableListOf()
+
+        gattServices.forEach { gattService ->
+            val currentServiceData = HashMap<String, String>()
+            uuid = gattService.uuid.toString()
+            currentServiceData[LIST_NAME] = gattService.toString()
+            currentServiceData[LIST_UUID] = uuid!!
+            gattServiceData += currentServiceData
+
+            val gattCharacteristicGroupData: ArrayList<HashMap<String, String>> = arrayListOf()
+            val gattCharacteristics = gattService.characteristics
+            val charas: MutableList<BluetoothGattCharacteristic> = mutableListOf()
+
+            gattCharacteristics.forEach { gattCharacteristic ->
+                charas += gattCharacteristic
+                val currentCharaData: HashMap<String, String> = hashMapOf()
+                uuid = gattCharacteristic.uuid.toString()
+                currentCharaData[LIST_NAME] = gattCharacteristic.toString()
+                currentCharaData[LIST_UUID] = uuid!!
+                gattCharacteristicGroupData += currentCharaData
+            }
+
+            mGattCharacteristics += charas
+            gattCharacteristicData += gattCharacteristicGroupData
+        }
+
+        Timber.tag("displayGattService").d("GATT_CHARACTERISTICS: $mGattCharacteristics")
+        Timber.tag("displayGattService").d("GATT_CHARACTERISTICS_DATA: $gattCharacteristicData")
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    fun readCharacteristic(characteristic: BluetoothGattCharacteristic) {
+        bluetoothGatt?.readCharacteristic(characteristic)
+    }
+
+    private fun readStaticChar() {
+        if(::mGattCharacteristics.isInitialized) {
+            Timber.tag("ReadCharacteristic").d("CHARS_LAST_INDEX: ${mGattCharacteristics.lastIndex}")
+            Timber.tag("ReadCharacteristic").d("CHARS: $mGattCharacteristics")
+            if (mGattCharacteristics.isEmpty()) {
+                binding.root.snacked("No Characteristic available")
+            } else {
+                val characteristic = mGattCharacteristics[0]
+                Timber.tag("ReadCharacteristic").d("CHAR: $characteristic")
+                readCharacteristic(characteristic)
+            }
+        } else {
+            binding.root.snacked("No Characteristic available")
+        }
+        //Last Index: 25
+        //Can't read: Index -> [3, 14, 15, 18, 19, 21, 22, 23, 24, 25]
+
+        //Read: Index 0 -> {CHAR_VALUE: [B@ee33ab8 and 4D 36}
+        //Read: Index 1 -> {CHAR_VALUE: [B@353e07b and 00 00}
+        //Read: Index 2 -> {CHAR_VALUE: [B@d9c8e65 and 0F 00 0F 00 00 00 E8 03}
+        //Read: Index 4 -> {CHAR_VALUE: [B@1617617 and 03 00 00 00 B4 A5 FF 1F} -> Comparing
+        //Read: Index 4 -> {CHAR_VALUE: [B@3ca2d7 and 03 00 00 00 B4 A5 FF 1F} -> Comparing
+        //Read: Index 5 -> {CHAR_VALUE: [B@bbd3b7a and 4D 6F 64 65 6C 20 4E 75 6D 62 65 72}
+        //Read: Index 6 -> {CHAR_VALUE: [B@aa41194 and 53 65 72 69 61 6C 20 4E 75 6D 62 65 72}
+        //Read: Index 7 -> {CHAR_VALUE: [B@3a76063 and 46 69 72 6D 77 61 72 65 20 52 65 76 69 73 69 6F 6E}
+        //Read: Index 8 -> {CHAR_VALUE: [B@c8e1f39 and 48 61 72 64 77 61 72 65 20 52 65 76 69 73 69 6F 6E}
+        //Read: Index 9 -> {CHAR_VALUE: [B@4de67b5 and 53 6F 66 74 77 61 72 65 20 52 65 76 69 73 69 6F 6E}
+        //Read: Index 10 -> {CHAR_VALUE: [B@32fd503 and 4D 61 6E 75 66 61 63 74 75 72 65 72 20 4E 61 6D 65}
+        //Read: Index 11 -> {CHAR_VALUE: [B@3e0d193 and FE 00 65 78 70 65 72 69 6D 65 6E 74 61 6C}
+        //Read: Index 12 -> {CHAR_VALUE: [B@871e786 and 01 04 05 00 00 10 01}
+        //Read: Index 13 -> {CHAR_VALUE: [B@a7323da and 5F}
+        //Read: Index 16 -> {CHAR_VALUE: [B@528615e and AC 04 09 90 75 0D 00 01 01 31 97 00 00 00 00 00 00 00 00 00}
+        //Read: Index 17 -> {CHAR_VALUE: [B@5d48113 and 5A 14 12 00 17 07 03 00 00 08 2A 00 00 05 10 00 00 B6 40 96}
+        //Read: Index 20 -> {CHAR_VALUE: [B@ab425f3 and 01}
+
+    }
 
     companion object {
         const val REQUEST_PERMISSION_BLUETOOTH = 200
         const val TIME_REFRESH = 1000L
         const val TIME_SCANNING = 12000L
+        const val LIST_NAME = "name"
+        const val LIST_UUID = "uuid"
     }
 
 
